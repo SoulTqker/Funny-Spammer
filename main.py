1c5b05323cba863c1a9c212144bad6c292b01376
Wati-Dev <69645213+SoulTqker@users.noreply.github.com>
2020-12-05 12:06:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pyautogui, time
+print("Welcome to the Script Spammer, do you want to use it ? Y/N")
+answ = input("")
 
-time.sleep(20)  ##  YOU CAN CHANGE THE TIME BEFORE SPAMMING HERE
+if answ in ["Yes", "yes", "Y", "y"]:
+    start()
+if answ in ["No", "no", "N", "n"]:
+    print("Ok, ending the program...")
+    pass
+def start(time):
+    print(f"You have now {time} seconds to open the tchat box where you want to  spam")
+    time.sleep(time)
 
-f = open("Script.txt", "r")
-for words in f:
-    pyautogui.typewrite(words)
-    pyautogui.press("enter")
+    f = open("Script.txt", "r")
+    for words in f:
+        pyautogui.typewrite(words)
+        pyautogui.press("enter")
